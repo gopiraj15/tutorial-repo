061bc3860b5fc0e35e7b35e63c8b90f01d0c30aa
hari275 <harikrish275@gmail.com>
2019-07-29 09:09:36 +0530

    Update data_collection.py

--- data_collection.py
+++ data_collection.py
@@ -5,11 +5,11 @@ open_pg=req.get(base_url+"/sentences").text
 soup=BeautifulSoup(open_pg,"lxml")
 vid_title_url={}
 for i in soup.find("div",{"class":"span8"}).find_all("div",{"class":"views-field views-field-title"}):
-	
-	vid_pg=BeautifulSoup(req.get(base_url+i.find("a").get("href")).text,"lxml")
-	vid_title_url[i.text]=vid_pg.find("iframe").get("src").replace("//","")
+    vid_pg=BeautifulSoup(req.get(base_url+i.find("a").get("href")).text,"lxml")
+    vid_title_url[i.text]=vid_pg.find("iframe").get("src").replace("//","")
 
 from pytube import YouTube
 for i in vid_title_url.keys():
-	yt = YouTube(vid_title_url[i])
+    if vid_title_url[i]:
+        yt = YouTube(vid_title_url[i])
 	yt.streams.first().download(filename=i)
